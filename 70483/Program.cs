9f4c1dd92de310ed2c17018406dbc3d872567cc6
Juan Carlos Castro Socla <juan.castro.socla@gmail.com>
2019-06-11 20:32:40 -0500

    Create a Proyect. 1. Creating a thread with the Tread Class

---
+++ 70483/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace _70483
+{
+    public static class Program
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}" , i);
+                Thread.Sleep(60);
+            }
+        }
+        public static void Main()
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main Thread: Do some work");
+                Thread.Sleep(30);
+            }
+
+            t.Join();
+            Console.ReadLine();
+        }
+    }
+}
